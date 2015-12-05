7c6c90a964892647ddf7f97f1441345e5fcbdde7
Stefan Geyer <sgeyer@student.tgm.ac.at>
2015-12-05 15:56:25 +0100

    Added MongoDBConfig to beable to connect to a remote Mongo Database

---
+++ src/main/java/at/geyerritter/dezsys07/MongoDBConfig.java
@@ -0,0 +1,20 @@
+package at.geyerritter.dezsys07;
+
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+
+@Configuration
+public class MongoDBConfig extends AbstractMongoConfiguration {
+
+    @Override
+    public Mongo mongo() throws Exception {
+        return new MongoClient("192.168.10.200", 27017);
+    }
+
+    @Override
+    protected String getDatabaseName() {
+        return "testdb";
+    }
+}
