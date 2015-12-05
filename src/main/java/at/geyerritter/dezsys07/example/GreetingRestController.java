3b41e043c789815bdf441c80fc9ca56bc369b096
mritter-tgm <mritter@student.tgm.ac.at>
2015-12-05 14:32:20 +0100

    finished greeting example

---
+++ src/main/java/at/geyerritter/dezsys07/example/GreetingRestController.java
@@ -0,0 +1,22 @@
+package at.geyerritter.dezsys07.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingRestController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/api/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+
+}
