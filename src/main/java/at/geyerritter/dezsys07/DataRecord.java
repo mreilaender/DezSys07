8076b13e578bfe24bfb3312783b9cdda2600be17
Stefan Geyer <sgeyer@student.tgm.ac.at>
2015-12-05 15:36:12 +0100

    Added Repository, Service and DTO

--- src/main/java/at/geyerritter/dezsys07/DataRecord.java
+++ src/main/java/at/geyerritter/dezsys07/DataRecord.java
@@ -1,6 +1,11 @@
 package at.geyerritter.dezsys07;
 
 import org.springframework.data.annotation.Id;
+import org.thymeleaf.util.Validate;
+
+import static org.thymeleaf.util.Validate.isTrue;
+import static org.thymeleaf.util.Validate.notEmpty;
+import static org.thymeleaf.util.Validate.notNull;
 
 /**
  * This class contains the entity data.
@@ -21,9 +26,13 @@ public class DataRecord {
 
     public DataRecord() {}
 
-    public DataRecord(String name, String description) {
-        this.name = name;
-        this.description = description;
+    private DataRecord(Builder builder) {
+        this.name = builder.name;
+        this.description = builder.description;
+    }
+
+    public static Builder getBuilder() {
+        return new Builder();
     }
 
     @Override
@@ -32,6 +41,42 @@ public class DataRecord {
                 "name='" + name + '\'' +
                 ", description='" + description + '\'' +
                 '}';
+    }
+
+    static class Builder {
+
+        private String name;
+        private String description;
+
+        private Builder() {}
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder description(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public DataRecord build() {
+            DataRecord record = new DataRecord(this);
+
+            checkRecordData();
+
+            return record;
+        }
+
+        private void checkRecordData() {
+            notNull(this.name, "Name cannot be null");
+            notEmpty(this.name, "Name cannot be empty");
+            isTrue(this.name.length() <= MAX_LENGTH_NAME, "Name cannot be longer than " + MAX_LENGTH_NAME + " characters");
+
+            if (this.description != null) {
+                isTrue(this.description.length() <= MAX_LENGTH_DESCRIPTION, "Description cannot be longer than " + MAX_LENGTH_DESCRIPTION + " characters");
+            }
+        }
     }
 
     public String getId() {
