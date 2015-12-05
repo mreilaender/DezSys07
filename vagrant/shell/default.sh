d69f5c469ea65f616a7f2fdfee310828214c31f1
Stefan Geyer <sgeyer@student.tgm.ac.at>
2015-12-05 12:18:25 +0100

    Added vagrant to use mongodb

---
+++ vagrant/shell/default.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "### START default.sh ###"
+
+# installing apache, php and sqlite
+apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/debian wheezy/mongodb-org/3.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+apt-get update
+apt-get dist-upgrade -y
+
+sudo apt-get install -y mongodb-org
+
+echo "### END default.sh ###"
+
