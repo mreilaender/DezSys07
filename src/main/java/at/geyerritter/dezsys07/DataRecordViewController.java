034490e0e4a8f2eb2a667c771a9babd90abb0983
mritter-tgm <mritter@student.tgm.ac.at>
2015-12-11 13:29:43 +0100

    began with implementation of view of datarecords

--- src/main/java/at/geyerritter/dezsys07/DataRecordViewController.java
+++ src/main/java/at/geyerritter/dezsys07/DataRecordViewController.java
@@ -1,15 +1,24 @@
 package at.geyerritter.dezsys07;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 @Controller
 public class DataRecordViewController {
 
+    @Autowired
+    private DataRecordRestController restController;
+
     @RequestMapping(value = "/datarecords", method = RequestMethod.GET, produces = "text/html")
-    public String displayDataRecords() {
-        return "greeting";
+    public String displayDataRecords(Model model) {
+        List<DataRecordDTO> dataRecords = restController.findDataRecordsByName("").getBody();
+        model.addAttribute("dataRecords", dataRecords);
+        return "datarecords";
     }
 
 }
