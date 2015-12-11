0192ea51dd8afb6eedc49f30ac4c162c94628093
Mathias Ritter <mritter@student.tgm.ac.at>
2015-12-11 12:18:07 +0100

    added javadocs to rest controller

--- src/main/java/at/geyerritter/dezsys07/DataRecordRestController.java
+++ src/main/java/at/geyerritter/dezsys07/DataRecordRestController.java
@@ -11,20 +11,46 @@ import org.springframework.web.bind.annotation.*;
 import javax.json.Json;
 import java.util.List;
 
+/**
+ * This class represents a RESTful API to create/read/update/delete a data record object.
+ *
+ *
+ * @author Stefan Geyer
+ * @author Mathias Ritter
+ *
+ * @version 20151211.1
+ */
 @RestController
 public class DataRecordRestController {
 
     @Autowired
     private MongoDBDataRecordService service;
 
+    /**
+     * Handles GET requests of the URL "/datarecords"<br />
+     * This method is used to retrieve data records.
+     * The GET parameter name is used for filtering the result. If this parameter is not provided
+     * all results are returned.
+     *
+     * @param name Filter for field name
+     * @return List including retrieved data records + status 200 OK
+     */
     @RequestMapping(value = "/datarecords", method = RequestMethod.GET)
-    public ResponseEntity<List<DataRecordDTO>> findDataRecordsByName(@RequestParam(value = "search", defaultValue = "") String search) {
-        if (search.length() == 0)
+    public ResponseEntity<List<DataRecordDTO>> findDataRecordsByName(@RequestParam(value = "name", defaultValue = "") String name) {
+        if (name.length() == 0)
             return new ResponseEntity<>(service.findAll(), HttpStatus.OK);
         else
-            return new ResponseEntity<>(service.findByNameContainingIgnoreCase(search), HttpStatus.OK);
+            return new ResponseEntity<>(service.findByNameContainingIgnoreCase(name), HttpStatus.OK);
     }
 
+    /**
+     * Handles POST requests of the URL "/datarecords"<br />
+     * This method is used to create a new data record. Therefore the datarecord object
+     * needs to be specified in the request body.
+     *
+     * @param dataRecordDTO The data record object that will be created
+     * @return The created data record + status 201 CREATED
+     */
     @RequestMapping(value="/datarecords", method = RequestMethod.POST)
     public ResponseEntity<DataRecordDTO> createDataRecord(@RequestBody DataRecordDTO dataRecordDTO) {
 
@@ -35,12 +61,29 @@ public class DataRecordRestController {
         return new ResponseEntity<>(dataRecordDTO, HttpStatus.CREATED);
     }
 
-
+    /**
+     * Handles GET requests of the URL "/datarecords/{id}"<br />
+     * This method is used to retrieve a data record specified by the id. The id is part of the URL.<br />
+     * If the id does not exist an error is returned.
+     *
+     * @param id The id of the data record that will be retrieved
+     * @return the retrieved object + status 200 OK or an error + status 404 NOT FOUND
+     */
     @RequestMapping(value = "/datarecords/{id}", method = RequestMethod.GET)
     public ResponseEntity<DataRecordDTO> findDataRecord(@PathVariable String id) {
         return new ResponseEntity<>(service.findById(id), HttpStatus.OK);
     }
 
+    /**
+     * Handles PUT requests of the URL "/datarecords/{id}"<br />
+     * This method is used to update an existing data record specified by the id. The id is part of the URL.
+     * The new data record is part of the body. <br />
+     * If the id does not exist an error is returned.
+     *
+     * @param id The id of the data record that will be updated.
+     * @param newDataRecord The data record containing the new information
+     * @return the updated object + status 200 OK or an error + status 404 NOT FOUND
+     */
     @RequestMapping(value = "/datarecords/{id}", method = RequestMethod.PUT)
     public ResponseEntity<DataRecordDTO> updateDataRecord(@PathVariable String id, @RequestBody DataRecordDTO newDataRecord) {
         DataRecordDTO dataRecord = service.findById(id);
@@ -50,18 +93,37 @@ public class DataRecordRestController {
         return new ResponseEntity<>(service.update(dataRecord), HttpStatus.OK);
     }
 
+    /**
+     * This method is called if an exception occurs that should lead to a 400 BAD REQUEST error.
+     *
+     * @param ex Exception to handle
+     * @return A 400 BAD REQUEST response + detailed information
+     */
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<String> handleBadRequest(Exception ex) {
         return generateJsonErrorResponse(HttpStatus.BAD_REQUEST, ex);
     }
 
+    /**
+     * This method is called if an exception occurs that should lead to a 404 NOT FOUND error.
+     *
+     * @param ex Exception to handle
+     * @return A 404 NOT FOUND response + detailed information
+     */
     @ExceptionHandler(EmptyResultDataAccessException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<String> handleNotFound(Exception ex) {
         return generateJsonErrorResponse(HttpStatus.NOT_FOUND, ex);
     }
 
+    /**
+     * Generates a json error response with the given status code + exception.
+     *
+     * @param status http status code
+     * @param ex exception
+     * @return error response
+     */
     private ResponseEntity<String> generateJsonErrorResponse(HttpStatus status, Exception ex) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
