e6db6b3119263f50ce4e83be788ea4df3e47c948
Stefan Geyer <sgeyer@student.tgm.ac.at>
2015-12-12 15:46:36 +0100

    SOAP search now requires the name and will return 100 entries containing the name

--- src/main/java/at/geyerritter/dezsys07/soa/ObjectFactory.java
+++ src/main/java/at/geyerritter/dezsys07/soa/ObjectFactory.java
@@ -2,7 +2,7 @@
 // Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.2.7 generiert 
 // Siehe <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
-// Generiert: 2015.12.12 um 02:05:53 PM CET 
+// Generiert: 2015.12.12 um 03:43:07 PM CET 
 //
 
 
@@ -14,17 +14,18 @@ import javax.xml.bind.annotation.XmlRegistry;
 
 
 /**
- * This object contains factory methods for each
- * Java content interface and Java element interface
- * generated in the at.geyerritter.dezsys07.soa package.
- * <p>An ObjectFactory allows you to programatically
- * construct new instances of the Java representation
- * for XML content. The Java representation of XML
- * content can consist of schema derived interfaces
- * and classes representing the binding of schema
- * type definitions, element declarations and model
- * groups.  Factory methods for each of these are
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the at.geyerritter.dezsys07.soa package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
  * provided in this class.
+ * 
  */
 @XmlRegistry
 public class ObjectFactory {
@@ -32,12 +33,14 @@ public class ObjectFactory {
 
     /**
      * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: at.geyerritter.dezsys07.soa
+     * 
      */
     public ObjectFactory() {
     }
 
     /**
      * Create an instance of {@link GetDataRecordRequest }
+     * 
      */
     public GetDataRecordRequest createGetDataRecordRequest() {
         return new GetDataRecordRequest();
@@ -45,6 +48,7 @@ public class ObjectFactory {
 
     /**
      * Create an instance of {@link GetDataRecordResponse }
+     * 
      */
     public GetDataRecordResponse createGetDataRecordResponse() {
         return new GetDataRecordResponse();
@@ -52,6 +56,7 @@ public class ObjectFactory {
 
     /**
      * Create an instance of {@link DataRecord }
+     * 
      */
     public DataRecord createDataRecord() {
         return new DataRecord();
