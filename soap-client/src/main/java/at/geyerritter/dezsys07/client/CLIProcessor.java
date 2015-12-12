7dc0c21a2c7baf7b2b4e31aa60c42abf7de1627d
Stefan Geyer <sgeyer@student.tgm.ac.at>
2015-12-12 23:23:21 +0100

    client works and gives a related error message if something went wrong

--- soap-client/src/main/java/at/geyerritter/dezsys07/client/CLIProcessor.java
+++ soap-client/src/main/java/at/geyerritter/dezsys07/client/CLIProcessor.java
@@ -1,27 +1,66 @@
 package at.geyerritter.dezsys07.client;
 
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.Options;
+import org.apache.commons.cli.*;
+
+import static at.geyerritter.dezsys07.client.Client.*;
 
 public class CLIProcessor {
+
+    public void parseArguments(String[] args) throws ParseException {
+        CommandLineParser parser = new DefaultParser();
+        Options options = getOptions();
+
+        CommandLine line = parser.parse(options, args);
+
+        if (line.hasOption('n')) {
+            NAME = line.getOptionValue('n');
+        }
+
+        if (line.hasOption('h')) {
+            HOST = line.getOptionValue('h');
+        }
+
+        if (line.hasOption('p')) {
+            PORT = line.getOptionValue('p');
+        }
+
+        if (line.hasOption('o')) {
+            DUMP_PATH = line.getOptionValue('o');
+        }
+    }
 
     public Options getOptions() {
 
         Options options = new Options();
 
-        Option name = Option.builder().argName("n").longOpt("name")
-                .desc("The name that is going to be looked up").required().build();
+        Option name = Option.builder("n").argName("name").longOpt("name")
+                .desc("The name that is going to be looked up").required().hasArg().build();
 
-        Option host = Option.builder().argName("h").longOpt("host")
-                .desc("The ip or domain of the application server").build();
+        Option host = Option.builder("h").argName("host").longOpt("host")
+                .desc("The ip or domain of the application server").hasArg()
+                .optionalArg(true).build();
 
-        Option port = Option.builder().argName("p").longOpt("port")
-                .desc("The port where the application server is running on").build();
+        Option port = Option.builder("p").argName("port").longOpt("port")
+                .desc("The port where the application server is running on").hasArg()
+                .optionalArg(true).build();
+
+        Option output = Option.builder("o").argName("output").longOpt("output")
+                .desc("The file the SOAP response will be dumped to").hasArg()
+                .optionalArg(true).build();
 
         options.addOption(name);
         options.addOption(host);
         options.addOption(port);
+        options.addOption(output);
 
         return options;
+    }
+
+    public void printHelp() {
+        String header = "Client application to search the web service for entries";
+        String footer = "";
+
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp("dezsys07", header, getOptions(), footer, true);
     }
 }
--- soap-client/src/main/java/at/geyerritter/dezsys07/client/Client.java
+++ soap-client/src/main/java/at/geyerritter/dezsys07/client/Client.java
@@ -1,15 +1,43 @@
 package at.geyerritter.dezsys07.client;
 
+import org.apache.commons.cli.ParseException;
+
 import javax.xml.soap.SOAPMessage;
+import java.io.*;
 
 public class Client {
+
+    public static String NAME;
+
+    public static String HOST = "localhost";
+
+    public static String PORT = "8080";
+
+    public static String DUMP_PATH = "";
+
     public static void main(String args[]) {
+        CLIProcessor processor = new CLIProcessor();
+
         try {
-            SOAPMessageCreator messageCreator = new DezSysSOAPMessageCreator("Eric");
-            SOAPConnector connector = new SOAPConnector(messageCreator, "http://127.0.0.1:8080/datarecords/search");
+            processor.parseArguments(args);
+            SOAPMessageCreator messageCreator = new DezSysSOAPMessageCreator(NAME);
+            SOAPConnector connector = new SOAPConnector(messageCreator, "http://" + HOST + ":" + PORT + "/datarecords/search");
 
             SOAPMessage message = connector.call();
-            System.out.println(ClientUtils.soapMessageToString(message, true));
+            String messageString = ClientUtils.soapMessageToString(message, true);
+
+            if (DUMP_PATH.isEmpty()) {
+                System.out.println(messageString);
+            } else {
+                System.out.println("Dumping response to file: " + DUMP_PATH);
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(DUMP_PATH)));
+                writer.write(messageString);
+                writer.flush();
+                writer.close();
+            }
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            processor.printHelp();
         } catch (Exception e) {
             e.printStackTrace();
         }
