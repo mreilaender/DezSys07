09201d41af413c80a7e50357ee689918f1e76b21
mritter <mritter@student.tgm.ac.at>
2015-12-17 18:49:55 +0100

    beatufied pom

--- web-application/src/main/java/at/geyerritter/dezsys07/Application.java
+++ web-application/src/main/java/at/geyerritter/dezsys07/Application.java
@@ -5,8 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
+/**
+ * Entry point for the spring web application. <br>
+ * Do not run via IntelliJ, build with maven install and run with spring-boot:run
+ *
+ *
+ *
+ */
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
+
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
